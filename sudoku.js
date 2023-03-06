e4bcf35157811e0d5a8dcc24369099a78bbdc8a0
VladislavTatyankin <vladislavtatyankin01@gmail.com>
2023-03-06 13:33:14 +0300

    solve func

--- sudoku.js
+++ sudoku.js
@@ -4,6 +4,38 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  let flag = true;
+  let zN = 0;
+  for (let i = 0; i < resArrV.length; i++) {
+    for (let j = 0; j < resArrV.length; j++) {
+      if (resArrV[i][j] == "-") {
+        //проверка на символ -
+        if (zN === fillArrN[i].length-1) {
+          zN = 0;
+        }
+      //  console.log(i,j,zN);
+        for (let z = zN; z < fillArrN[i].length; z++) {
+          // цикл по fillArrN  с потенциальными значениями
+          if (flag != false) {
+            // условие чтобы повторно не перезаписывать
+            if (resArrV[i].indexOf(fillArrN[i][z]) == -1) {
+              if (updateBoard(resArrV)[j].indexOf(fillArrN[i][z]) == -1) {
+                resArrV[i][j] = fillArrN[i][z];
+                flag = false;
+                zN = z;
+              }
+            }
+          }
+        }
+       // zN = 0
+        flag = true;
+      }
+    }
+  }
+
+  //let arrRec = [...resArrV]; //перевернутый массив
+
+  return resArrV;
 }
 
 /**
